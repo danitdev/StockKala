1c2d4d5f0d3c5570599f61db57e27a42b99171c5
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-01 15:40:53 -0700

    feat: added relations

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 import express from "express";
+import path from "path";
 import bodyParser from "body-parser";
 import rootDir from "./utils/path.js";
 import { sequelize } from "./utils/database.js";
+import { Product } from "./models/product.js";
+import { Category } from "./models/category.js";
+
 
 const app = express();
 
 app.set("view engine","ejs");
 app.set("views","views")
-app.use(bodyParser.urlencoded({extend:false}));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(rootDir,"public")));
+
+
+Product.belongsTo(Category);
+Category.hasMany(Product);
+
+
 
 sequelize.sync({force:true})
     .then(result=>{
