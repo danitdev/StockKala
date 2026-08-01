1eaa2cc10894e4136ce47aa3ad137ca999f84b64
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-01 15:00:20 -0700

    feat: add path.js to have acess to root dir

---
+++ utils/path.js
@@ -0,0 +1,9 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname_utils = path.dirname(__filename);
+const __dirname_project = path.dirname(__dirname_utils);
+
+export default __dirname_project;
