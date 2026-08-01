3d21c75b36f0b56532ce9569a8d50f522b6e8aeb
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-01 15:34:49 -0700

    feat: add category model

---
+++ models/category.js
@@ -0,0 +1,14 @@
+import {Sequelize, DataTypes} from "sequelize";
+import { sequelize } from "../utils/database";
+
+export const Category = sequelize.define("category",{
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+    type: DataTypes.STRING,
+    allowNull: false
+    }
+});
