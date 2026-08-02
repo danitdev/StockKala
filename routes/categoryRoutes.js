e3bcaa39c7fb7723816575f4857deab6a0d1b1ba
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-01 22:09:03 -0700

    feat: add postEditCategory route to category router

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getCategories, getEditCategories } from "../controllers/categoryController.js";
+import { getCategories, getEditCategories, postEditCategories } from "../controllers/categoryController.js";
 const router = express.Router();
 
 
 
 router.get("/",getCategories);
 router.get("/edit/:categoryId",getEditCategories);
+router.post("/edit/:categoryId",postEditCategories);
 export{router};
