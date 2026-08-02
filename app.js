fa06137a28b7219301f42561c728f960bde32d10
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-01 21:34:58 -0700

    feat: seed categories

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
 import rootDir from "./utils/path.js";
+import { router as categoryRouter} from "./routes/categoryRoutes.js";
 import { sequelize } from "./utils/database.js";
 import { Product } from "./models/product.js";
 import { Category } from "./models/category.js";
-
+import { seedCategories } from "./seeders/categorySeeder.js";
 
 const app = express();
 
@@ -15,13 +16,18 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(rootDir,"public")));
 
 
+app.use("/categories",categoryRouter);
+
 Product.belongsTo(Category);
 Category.hasMany(Product);
 
 
 
-sequelize.sync({force:true})
-    .then(result=>{
+sequelize.sync({force:false})
+    .then(()=>{
+        return seedCategories();
+    })
+    .then(()=>{
         app.listen(3000);
     })
     .catch(err=>{console.log(err);})
