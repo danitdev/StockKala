afe543990b53af76b2b3b52720d12cd247a3061f
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-01 22:07:48 -0700

    feat: add editing categories in controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -10,7 +10,24 @@ export const getEditCategories = (req,res,next)=>{
     const categoryId = req.params.categoryId;
     Category.findByPk(categoryId)
         .then(category=>{
+            if(!category) return res.redirect("/");
             res.render("categories/edit-category",{pageTitle:"Edit Category",path:"/categories",category});
         })
         .catch(err=>console.log(err));
+};
+export const postEditCategories = (req,res,next)=>{
+    const categoryId = req.params.categoryId;
+    const updatedName = req.body.name;
+    console.log(updatedName);
+    console.log(categoryId);
+    Category.findByPk(categoryId)
+        .then(category=>{
+            category.name = updatedName;
+            return category.save();
+        })
+        .then(result=>{
+            console.log("Updated CATEGORY!");
+            res.redirect("/categories");
+        })
+        .catch(err => console.log(err));
 };
