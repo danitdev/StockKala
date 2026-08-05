72e63d9a87a9405c52efcfd349161153e5338e56
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-04 23:51:01 -0700

    feat: add product routes file and add getProducts

---
+++ routes/productRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { getProducts } from "../controllers/productController";
+
+
+const router = express.Router();
+
+router.get("/",getProducts);
+
+export{router};
