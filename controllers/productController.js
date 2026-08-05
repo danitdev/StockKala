a6f77365f259c8f2ac2b68fcd928cf95a8841c0c
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-05 00:32:32 -0700

    feat: add post create product to product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,3 +9,21 @@ export const getProducts = (req,res,next)=>{
 export const getCreateProduct = (req,res,next)=>{
     res.render("products/create",{pageTitle:"Add Product",path:"/products/create"});
 };
+export const postCreateProduct = (req,res,next)=>{
+    const name = req.body.name;
+    const sku = req.body.sku;
+    const price = req.body.price;
+    const stockQuantity = req.body.stockQuantity;
+    const description = req.body.description;
+    const imageUrl = req.body.imageUrl;
+    Product.create({
+        name,
+        sku,
+        price,
+        stockQuantity,
+        description,
+        imageUrl
+    })
+        .then(()=>res.redirect("/products"))
+        .catch(err=>console.log(err));
+};
