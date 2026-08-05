b6b0aed0b58c8235ab9e2fc625a839b3df766b06
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-05 00:03:28 -0700

    feat: add index.ejs for products page

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,5 @@
 import {Product} from "../models/product.js"
 export const getProducts = (req,res,next)=>{
-    // Category.findAll()
-    //     .then(categories=>{
-    //         res.render("categories/list",{categories:categories,pageTitle:"All Categories",path:"/categories"});
-    //     })
-    //     .catch(err=>console.log(err));
     Product.findAll()
         .then(products=>{
             res.render("products/index",{products:products,pageTitle:"Products",path:"/products"});
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getProducts } from "../controllers/productController";
+import { getProducts } from "../controllers/productController.js";
 
 
 const router = express.Router();
