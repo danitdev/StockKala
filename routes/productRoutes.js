ea5b07ff50934de9972334df17a58bb527658f42
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-05 00:16:10 -0700

    feat: add getCreateProduct route to product router

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getProducts } from "../controllers/productController.js";
+import { getCreateProduct, getProducts } from "../controllers/productController.js";
 
 
 const router = express.Router();
 
 router.get("/",getProducts);
-
+router.get("/create",getCreateProduct);
 export{router};
