808ae864ca0272585119c5030a6c1ef94e968459
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-05 00:32:49 -0700

    feat: add post product to product router

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getCreateProduct, getProducts } from "../controllers/productController.js";
+import { getCreateProduct, getProducts, postCreateProduct } from "../controllers/productController.js";
 
 
 const router = express.Router();
 
 router.get("/",getProducts);
 router.get("/create",getCreateProduct);
+router.post("/create",postCreateProduct);
+
 export{router};
